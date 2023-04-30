2ca49a76fcf780b774be030bb48c525d48143293
shandyz <shandyztop@hotmail.com>
2023-04-30 20:38:59 +0200

    done

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,18 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        boolean isLeapYear;
+
+        // divisible by 4
+        isLeapYear = (year % 4 == 0);
+
+        // divisible by 4, not by 100, or divisible by 400
+        isLeapYear = isLeapYear && (year % 100 != 0 || year % 400 == 0);
+
+        if (isLeapYear) {
+            System.out.println("leap");
+        } else {
+            System.out.println("not leap");
+        }
     }
 }
